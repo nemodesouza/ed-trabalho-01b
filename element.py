43ccdd3567a7e571d12f8358cb5f2235dde32d66
Nemo de Souza <nemodesouza@gmail.com>
2021-11-19 16:15:58 -0300

    stack reviewed

--- element.py
+++ element.py
@@ -4,4 +4,18 @@ class Element:
         self.__data = data
         self.__prev = None
 
-    
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def prev(self):
+        return self.__prev
+
+    @data.setter
+    def data(self, data):
+        self.__data = data
+
+    @prev.setter
+    def prev(self, prev):
+        self.__prev = prev
